6cae72b3a68af0db8ac12c4cb354866826b46110
Arshdeep <achhokar@symposify.io>
2022-05-05 09:19:44 -0700

    auth

---
+++ src/components/Profile/ProfileForm.js
@@ -0,0 +1,75 @@
+import { useRef, useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
+
+const ProfileForm = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const history = useHistory();
+
+  const newPasswordInputRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredPassword = newPasswordInputRef.current.value;
+
+    setIsLoading(true);
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyARstToI9Wn7NYV7dlSJvIPp6-rcv3OIX0",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: authCtx.token,
+          password: enteredPassword,
+          returnSecureToken: false,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        if (response.ok) {
+          return response.json();
+        } else {
+          return response.json().then((data) => {
+            let errorMessage = "Authentication Failed!";
+
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        history.replace("/");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <div className={classes.control}>
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" ref={newPasswordInputRef} />
+      </div>
+      <div className={classes.action}>
+        {!isLoading && <button>Change Password</button>}
+        {isLoading && <p>Sending request...</p>}
+      </div>
+    </form>
+  );
+};
+
+export default ProfileForm;
